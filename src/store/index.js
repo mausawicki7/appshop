907e378481edd708f880cf23c324b5c5f32635b2
mausawicki7 <mau.sawicki@gmail.com>
2022-02-03 12:53:16 -0300

    Implementación de Redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore, combineReducers } from "redux";
-
+import { createStore, combineReducers } from "redux"; //Importacion del Store y Combinacion de Reducers
 
 import CategoryReducer from "./reducers/category.reducer";
 import BreadsReducer from "./reducers/breads.reducer";
